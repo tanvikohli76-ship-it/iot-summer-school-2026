85f23468cf4b52c2f0d43133fb41b521c5dbf47a
Tanvi Kohli <tanvikohli76@gmail.com>
2026-06-27 15:47:12 +0530

    feat: v1.1 added serial print for blink count

--- week1/led_blink/ledblink.ino
+++ week1/led_blink/ledblink.ino
@@ -1,10 +1,15 @@
 int led = 13;
+int count = 0;
 void setup() {
   pinMode(led, OUTPUT);
+  Serial.begin(9600);
 }
 void loop() {
   digitalWrite(led, HIGH);
   delay(500);
   digitalWrite(led, LOW);
   delay(500);
+  count++;
+  Serial.print("Blink count: ");
+  Serial.println(count);
 }
