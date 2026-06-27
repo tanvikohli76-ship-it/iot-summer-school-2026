303e0a63722ab354e3362ee1a07e90e8684225d2
Tanvi Kohli <tanvikohli76@gmail.com>
2026-06-27 16:52:36 +0530

    Fixes #1 improved potentiometer mapping for better delay response

--- week1/led_blink/ledblink.ino
+++ week1/led_blink/ledblink.ino
@@ -13,7 +13,7 @@ void setup() {
 }
 void loop() {
   int value = analogRead(pot);
-  int delayTime = map(value, 0, 1023, 100, 1000);
+  int delayTime = map(value, 0, 1023, 50, 2000);
   digitalWrite(led, HIGH);
   delay(delayTime);
   digitalWrite(led, LOW);
