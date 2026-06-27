60b43c5942104b1614bfef8dbbb18d638fece1fa
Tanvi Kohli <tanvikohli76@gmail.com>
2026-06-27 15:46:01 +0530

    feat: v1.0 basic led blink with 500 ms interval

--- week1/led_blink/ledblink.ino
+++ week1/led_blink/ledblink.ino
@@ -1,9 +1,10 @@
-void setup() { 
-pinMode(13, OUTPUT); 
-} 
-void loop() { 
-digitalWrite(13, HIGH); 
-delay(1000); 
-digitalWrite(13, LOW); 
-delay(1000); 
-} 
+int led = 13;
+void setup() {
+  pinMode(led, OUTPUT);
+}
+void loop() {
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(500);
+}
