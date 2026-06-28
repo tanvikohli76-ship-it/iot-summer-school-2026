152e85289c2346a4ff3cb47da2c23bb246b0dc93
Tanvi Kohli <tanvikohli76@gmail.com>
2026-06-28 20:01:52 +0530

    Create wokwi.ino
    Simulation without wifi on wokwi

---
+++ week4/wifi_webserver/wokwi.ino
@@ -0,0 +1,29 @@
+#include <DHTesp.h>
+
+#define DHT_PIN 15
+#define LED_PIN 2
+
+DHTesp dht;
+bool ledState = false;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED_PIN, OUTPUT);
+  dht.setup(DHT_PIN, DHTesp::DHT22);
+}
+
+void loop() {
+  float temp = dht.getTemperature();
+  float hum = dht.getHumidity();
+
+  Serial.print("Temp: ");
+  Serial.print(temp);
+  Serial.print(" C | Humidity: ");
+  Serial.println(hum);
+
+  // Simulate LED toggle every 5 sec
+  ledState = !ledState;
+  digitalWrite(LED_PIN, ledState);
+
+  delay(3000);
+}
